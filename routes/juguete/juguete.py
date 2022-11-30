f96bd301cd742628014ca57fc269e4ce143fae1b
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2022-11-30 12:51:47 -0600

    Se agrego insertar juguetes

--- routes/juguete/juguete.py
+++ routes/juguete/juguete.py
@@ -1,44 +1,43 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, Flask, request, url_for, render_template, redirect, jsonify, make_response
 from sqlalchemy import exc
 from models import User,Proveedor,Juguete
 from app import db, bcrypt
 from auth import tokenCheck
+from forms import JugueteForm
 appjuguete = Blueprint('appjuguete',__name__,template_folder="template")
 
-@appjuguete.route('/agregarjuguetes', methods=['POST'])
+@appjuguete.route('/agregarjuguetes', methods=['POST','GET'])
 @tokenCheck
 def registro(usuario):
-    juguete = request.get_json()
-    jugueteExist = Juguete.query.filter_by(nombre=juguete['nombre']).first()
-    proveedorExist= Proveedor.query.filter_by(id=juguete['proveedor_id']).first()
-    if not jugueteExist:
-        if proveedorExist:
-            agregarj = Juguete(nombre=juguete['nombre'], costo=juguete['costo'], cantidad=juguete['cantidad'],proveedor_id=juguete['proveedor_id'])
-            try:
-                db.session.add(agregarj)
-                db.session.commit()
-                mensaje = "Juguete agregado"
-            except exc.SQLAlchemyError as e:
-                mensaje = print(e)
-        else:
-            mensaje= "El proveedor no existe"
-    else:
-        mensaje="El juguete ya existe"
-    return jsonify({"mensaje":mensaje})
+    mensaje = "Registro de juguetes"
+    jugueteForma = JugueteForm()
+    if request.method == "POST":
+        if jugueteForma.validate_on_submit():
+            juguete = {"nombre":jugueteForma.nombre.data, "costo":jugueteForma.costo.data,"cantidad": jugueteForma.cantidad.data,"proveedor_id": jugueteForma.proveedor_id.data}
+            jugueteExist = Juguete.query.filter_by(nombre=juguete['nombre']).first()
+            proveedorExist= Proveedor.query.filter_by(id=juguete['proveedor_id']).first()
+            if not jugueteExist:
+                if proveedorExist:
+                    agregarj = Juguete(nombre=juguete['nombre'], costo=juguete['costo'], cantidad=juguete['cantidad'],proveedor_id=juguete['proveedor_id'])
+                    try:
+                        db.session.add(agregarj)
+                        db.session.commit()
+                        mensaje = "Juguete agregado"
+                    except exc.SQLAlchemyError as e:
+                        mensaje = print(e)
+                else:
+                    mensaje= "El proveedor no existe"
+            else:
+                mensaje="El juguete ya existe"
+            return render_template('juguete/agregarjuguete.html', forma=jugueteForma, mensaje=mensaje)
+    return render_template('juguete/agregarjuguete.html', forma=jugueteForma, mensaje=mensaje)
 
 @appjuguete.route('/juguetes', methods=["GET"])
 @tokenCheck
-def getUsers(usuario):
-    print (usuario)
+def getJuguete(usuario):
     if usuario['admin']:
         output=[]
         juguetes = Juguete.query.all()
-        for juguete in juguetes:
-            obj = {}
-            obj['id']=juguete.id
-            obj['nombre']=juguete.nombre
-            obj['costo']=juguete.costo
-            obj['cantidad'] = juguete.cantidad
-            obj['proveedor_id'] = juguete.proveedor_id
-            output.append(obj)
-        return jsonify({'proveedor':output})
+        return render_template('juguete/juguete.html', juguetes=juguetes)
+    else:
+        return jsonify({"mensaje": "Es necesario tener permisos de administrador"})
